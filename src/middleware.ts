42dc341fcfb3c6e8eaaad4542ef9c2dca31ce2f7
nazim1971 <md.nazimuddinaj@gmail.com>
2025-05-22 02:51:15 +0600

    Work middleware

---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getCurrentUser } from './services/AuthService';
+
+type Role = keyof typeof roleBasedPrivateRoutes;
+
+const authRoutes = ['/'];
+
+const roleBasedPrivateRoutes = {
+  ADMIN: [/^\/admin/],
+};
+
+export const middleware = async (request: NextRequest) => {
+  const { pathname, origin } = request.nextUrl;
+
+  const userInfo = await getCurrentUser();
+
+  if (!userInfo) {
+    if (authRoutes.includes(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(`${origin}/?redirectPath=${pathname}`);
+    }
+  }
+
+  if (userInfo?.role && roleBasedPrivateRoutes[userInfo?.role as Role]) {
+    const routes = roleBasedPrivateRoutes[userInfo?.role as Role];
+    if (routes.some(route => pathname.match(route))) {
+      return NextResponse.next();
+    }
+  }
+
+  return NextResponse.redirect(new URL('/', request.url));
+};
+
+export const config = {
+  matcher: [
+    '/admin',
+    '/admin/:page*',
+  ],
+};
